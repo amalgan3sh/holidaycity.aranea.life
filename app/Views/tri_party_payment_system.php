b37e88a27f5e22c877472db12ac28ffd86eb8fe2
ansonanroy <ansonanroy2022a@mca.ajce.in>
2024-09-25 05:33:10 +0530

    tri party

--- app/Views/tri_party_payment_system.php
+++ app/Views/tri_party_payment_system.php
@@ -195,8 +195,8 @@
                 <div class="footer-col footer-subscribe-col">
                     <div class="footer-widget">
                         <h4>Subscribe Eco City and get -20% off</h4>
-                        <p>Aranea presents you Nest- HolidayCity for you and your dear ones. Eco City
-                            introducing you the customized resorts, villas and farmhouse from your wish-list.</p>
+                        <p>Aranea presents you Nest- Eco City for you and your dear ones. Eco City
+                        introducing you the customized resorts, villas and farmhouse from your wish-list.</p>
                         <form class="footer-subscribe-form">
                             <div class="input-wrapper">
                                 <input type="email" placeholder="Type your address email...">
